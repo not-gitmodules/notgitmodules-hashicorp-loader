aaaa4c9f71634839e2c1da64e47c49749195472e
Armen-Jean Andreasian <armen_andreasian@proton.me>
2025-02-14 00:10:27 +0700

    Update

--- loader.py
+++ loader.py
@@ -1,10 +1,54 @@
 import time
 import requests
 import os
-from utils.file_manager import FileManager
+from typing import Union
 
 
-class HashiCorpLoader:
+class LoaderHelpers:
+    @staticmethod
+    def _obtain_api_token(client_id, client_secret, api_token_url):
+        """Obtains the API token from HashiCorp"""
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+        data = {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "client_credentials",
+            "audience": "https://api.hashicorp.cloud"
+        }
+
+        response = requests.post(url=api_token_url, headers=headers, data=data)
+
+        if response.status_code == 200:
+            return response.json().get("access_token")
+        raise RuntimeError("Hashicorp secret loading was failed")
+
+    @staticmethod
+    def _fetch_remote_secrets(secrets_url, api_token):
+        """Fetches secrets from Hashicorp"""
+        response = requests.get(secrets_url, headers={"Authorization": f"Bearer {api_token}"})
+
+        try:
+            return response.json()['secrets']
+        except Exception as e:
+            raise RuntimeError(f"Error occurred while retrieving data from Hashcorp vault: {str(e)}")
+
+    @staticmethod
+    def _prepare_dump(folder_to_save_dump: str) -> str:
+        dump_file = os.path.join(folder_to_save_dump, '.env.dump')
+
+        os.makedirs(os.path.dirname(folder_to_save_dump), exist_ok=True)
+
+        if not os.path.exists(folder_to_save_dump):
+            os.makedirs(folder_to_save_dump)
+
+        open(dump_file, "w").close()  # creating/overwriting the file
+        return dump_file
+
+
+class HashiCorpLoader(LoaderHelpers):
     """
     To use this class you need to set the following environment variables:
         HCP_CLIENT_ID
@@ -23,33 +67,18 @@ class HashiCorpLoader:
     It optionally can dump secrets to .env.dump file.
     """
 
-    def __obtain_api_token(self):
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-
-        data = {
-            "client_id": self.client_id,
-            "client_secret": self.client_secret,
-            "grant_type": "client_credentials",
-            "audience": "https://api.hashicorp.cloud"
-        }
-
-        response = requests.post(url=self.api_token_url, headers=headers, data=data)
-
-        if response.status_code == 200:
-            return response.json().get("access_token")
-        raise RuntimeError("Hashicorp secret loading was failed")
-
     def __init__(
         self,
-        dump_env_to_file: bool = False,
-        folder_to_save_env_dump: str = None,
+        folder_to_save_dump: str = None,
+        save_dump: bool = True,
     ):
         """
         param dump_to_env_file: bool - if True, remote secrets will also be dumped .env.dump file
         param folder_to_save_dump: str - path to the folder where .env.dump file will be saved
         """
+        if save_dump:
+            assert isinstance(folder_to_save_dump, str), f"folder_to_save_dump be a path, not {folder_to_save_dump}!"
+
         self.client_id: str = os.environ.get('HCP_CLIENT_ID')
         self.client_secret: str = os.environ.get('HCP_CLIENT_SECRET')
         self.api_token_url: str = os.environ.get('HCP_API_TOKEN_URL')
@@ -61,61 +90,44 @@ class HashiCorpLoader:
             )
 
         # set dump settings
-        self.dump_env_to_file = dump_env_to_file
-        self.folder_to_save_dump = folder_to_save_env_dump
+        self.save_dump = save_dump
+        self.folder_to_save_dump = folder_to_save_dump
 
-        self.__api_token = self.__obtain_api_token()
+        self.__api_token = self._obtain_api_token(client_id=self.client_id, client_secret=self.client_secret,
+                                                  api_token_url=self.api_token_url)
 
-    def __retrieve_remote_secrets(self) -> list[dict]:
-        response = requests.get(self.secrets_url, headers={"Authorization": f"Bearer {self.__api_token}"})
-
-        try:
-            return response.json()['secrets']
-        except Exception as e:
-            raise RuntimeError(f"Error occurred while retrieving data from Hashcorp vault: {str(e)}")
-
-    def load(self):
+    def load(self, debug: bool = False) -> Union[None, dict]:
         """
         This method loads HashiCorp secrets to environment variables.
+
+        If debug is none, means
+
         """
         if not os.environ.get('hcp_loaded'):
-            if self.dump_env_to_file:
-                dump_file = os.path.join(self.folder_to_save_dump, '.env.dump')
-
-                os.makedirs(os.path.dirname(self.folder_to_save_dump), exist_ok=True)
-
-                if not os.path.exists(self.folder_to_save_dump):
-                    os.makedirs(self.folder_to_save_dump)
-
-                open(dump_file, "w").close() # creating/overwriting the file
-
-                secrets_for_dump = dict()
-
             # retrieving secrets
-            remote_secrets: list[dict] = self.__retrieve_remote_secrets()
+            response: list[dict] = self._fetch_remote_secrets(secrets_url=self.secrets_url, api_token=self.__api_token)
+            remote_secrets = {secret_pair['name']: secret_pair['static_version']['value'] for secret_pair in response}
 
             # loading to env
-
-            for secret_pair in remote_secrets:
-                k, v = secret_pair['name'], secret_pair['static_version']['value']
+            for k, v in remote_secrets.items():
                 os.environ[k] = v
 
-                if self.dump_env_to_file:
-                    secrets_for_dump[k] = v
+            # .env.dump file
+            if self.save_dump:
+                dump_file: str = self._prepare_dump(folder_to_save_dump=self.folder_to_save_dump)
 
-            first = True
-            if self.dump_env_to_file:
-                for k, v in secrets_for_dump.items():
-                    if first:
-                        FileManager.append(file_path=dump_file, content=f"{k}={v}")
-                        first = False
-                    else:
-                        FileManager.append(file_path=dump_file, content=f"\n{k}={v}")
+                with open(dump_file, "a") as f:
+                    for i, (k, v) in enumerate(remote_secrets.items()):  # first break-line stuff
+                        f.write(f"{k}={v}") if i == 0 else f.write(f"\n{k}={v}")
                 time.sleep(2)  # waiting to write the file
 
             # setting env_loaded to any value to indicate it has already been loaded
             os.environ['hcp_loaded'] = '1'
 
             print("HashiCorp secrets are successfully loaded to environment!")
+
+            if debug:
+                return response
+
         else:
             print("HashiCorp secrets were already loaded. Skipping...")
